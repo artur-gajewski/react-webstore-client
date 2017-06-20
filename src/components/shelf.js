731db4f68ddfe71583eceff86409b3a8efd086f9
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-06-20 16:09:27 +0300

    Styled components for shelf

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import AlertContainer from 'react-alert';
-import './shelf.css';
 
 class Shelf extends Component {
     constructor(props) {
@@ -25,28 +25,74 @@ class Shelf extends Component {
 
         const shelfItems = this.props.shelfItems.map((product, idx) => {
             return (
-                <div key={product.productId} className="product-card">
-                    <div className="product-image">
-                        <img role="presentation" src={product.imageUrl} />
-                    </div>
-                    <div className="product-info">
+                <ProductCard key={product.productId}>
+                    <ProductImageContainer>
+                        <ProductImage role="presentation" src={product.imageUrl} />
+                    </ProductImageContainer>
+                    <ProductInfo>
                         <h5>{product.title}</h5>
                         <h6>{product.price.toFixed(2)} €</h6>
                         <h7>
-                            <button className="add-button" onClick={() => this.onAddProductToCart(product)}> Add to cart </button>
+                            <AddButton onClick={() => this.onAddProductToCart(product)}> Add to cart </AddButton>
                         </h7>
-                    </div>
-                </div>
+                    </ProductInfo>
+                </ProductCard>
             );
         });
 
         return (
-            <section className="products">
+            <Products>
                 <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
                 {shelfItems}
-            </section>
+            </Products>
         );
     }
 }
+
+const ProductCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    flex: 1 16%;
+    background-color: #FFF;
+    box-shadow: 0px 0px 1px 0px rgba(0,0,0,0.25);
+
+    @media ( max-width: 920px ) {
+        flex: 1 21%;
+    }
+
+    @media ( max-width: 600px ) {
+	    flex: 1 46%;
+    }
+`;
+
+const ProductImageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ProductImage = styled.img`
+    max-width: 200;
+    max-height: 200px;
+`;
+
+const ProductInfo = styled.div`
+    margin-top: auto;
+    padding-top: 20px;
+    text-align: center;
+`;
+
+const AddButton = styled.button`
+    margin-top: 10px;
+    font-size: 15px;
+`;
+
+const Products = styled.section`
+    display: flex;
+    -webkit-flex-wrap: wrap;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+`;
 
 export default Shelf;
