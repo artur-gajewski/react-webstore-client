331c82a2d5d1ca4751f42173f38d1a854370b681
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-06-21 08:06:08 +0300

    Added styled components for contact form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Icon } from 'react-fa';
-import './contactForm.css';
+import styled from 'styled-components';
 
 class ContactForm extends Component {
     render() {
         const { handleSubmit } = this.props;
         return (
-            <form className="contact-form" onSubmit={handleSubmit}>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="firstName">First name:</label>
-                    <Field className="form-input" name="firstName" component="input" type="text"/>
-                </div>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="lastName">Last name:</label>
-                    <Field className="form-input" name="lastName" component="input" type="text"/>
-                </div>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="email">Email:</label>
-                    <Field className="form-input" name="email" component="input" type="email"/>
-                </div>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="streetAddress">Street address:</label>
-                    <Field className="form-input" name="streetAddress" component="input" type="text"/>
-                </div>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="zipCode">Zip code:</label>
-                    <Field className="form-input" name="zipCode" component="input" type="text"/>
-                </div>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="city">City:</label>
-                    <Field className="form-input" name="city" component="input" type="text"/>
-                </div>
-                <div className="form-row">
-                    <label className="form-label" htmlFor="country">Country:</label>
-                    <Field className="form-input" name="country" component="input" type="text"/>
-                </div>
-                <div className="form-options">
-                    <button className="form-submit-button" type="submit">
+            <Form onSubmit={handleSubmit}>
+                <FormRow>
+                    <Label htmlFor="firstName">First name:</Label>
+                    <Input name="firstName" component="input" type="text"/>
+                </FormRow>
+                <FormRow>
+                    <Label htmlFor="lastName">Last name:</Label>
+                    <Input name="lastName" component="input" type="text"/>
+                </FormRow>
+                <FormRow>
+                    <Label htmlFor="email">Email:</Label>
+                    <Input name="email" component="input" type="email"/>
+                </FormRow>
+                <FormRow>
+                    <Label htmlFor="streetAddress">Street address:</Label>
+                    <Input name="streetAddress" component="input" type="text"/>
+                </FormRow>
+                <FormRow>
+                    <Label htmlFor="zipCode">Zip code:</Label>
+                    <Input name="zipCode" component="input" type="text"/>
+                </FormRow>
+                <FormRow>
+                    <Label htmlFor="city">City:</Label>
+                    <Input name="city" component="input" type="text"/>
+                </FormRow>
+                <FormRow>
+                    <Label htmlFor="country">Country:</Label>
+                    <Input name="country" component="input" type="text"/>
+                </FormRow>
+                <FormRow>
+                    <SubmitButton type="submit">
                         <Icon name="check-square-o" />
                         &nbsp;&nbsp;
                         Send order
-                    </button>
-                </div>
-            </form>
+                    </SubmitButton>
+                </FormRow>
+            </Form>
         );
     }
 }
@@ -52,5 +52,42 @@ class ContactForm extends Component {
 ContactForm = reduxForm({
     form: 'contact' // a unique name for this form
 })(ContactForm);
+
+const Form = styled.form`
+    display: flex;
+    flex-flow: column wrap;
+`;
+
+const FormRow = styled.div`
+    display: flex;
+    flex-grow: 1;
+    height: 20px;
+    margin-bottom: 10px;
+
+    @media ( max-width: 700px ) {
+        display: flex;
+        flex-direction: column;
+        height: 40px;
+    }
+`;
+
+const Label = styled.label`
+    flex: 1;
+`;
+
+const Input = styled(Field)`
+    flex: 4;
+`;
+
+const SubmitButton = styled.button`
+    margin-top: 15px;
+    font-size: 15px;
+    font-weight: 200;
+
+    @media ( min-width: 700px ) {
+        height: 25px;
+    }
+`;
+
 
 export default ContactForm;
